d812a52d3156c2bac2ac8c3ead5c5e0ed5a829ab
CaahOliveira <cassia.thauane@hotmail.com>
2025-03-21 22:16:15 -0300

    ValhaMinhaNossaSenhora

--- ts/main.ts
+++ ts/main.ts
@@ -4,15 +4,43 @@ namespace empresa{
     const campoNome = document.getElementById("campoNome") as HTMLInputElement;
     const campoAno = document.getElementById("campoAno") as HTMLInputElement;
 
+    const campoDeposito = document.getElementById("campoDeposito") as HTMLInputElement;
+
+    const btnDeposito = document.getElementById("deposito") as HTMLButtonElement;
+
+    const campoCodigo = document.getElementById("campoCodigo") as HTMLInputElement;
+    
+    let p: Cliente;
+
+    const campoCompra = document.getElementById("campoCompra") as HTMLButtonElement;
+    const btnComprar = document.getElementById("comprar") as HTMLButtonElement;
+
     calc.addEventListener("click", ()=>{
-        let p = new Pessoa();
+        p = new Cliente(parseInt(campoCodigo.value));
         p.nome = campoNome.value;
         p.anoNasc = parseInt(campoAno.value);
     
         document.getElementById("nome").textContent = p.nome;
         document.getElementById("ano").textContent = p.anoNasc.toString();
         document.getElementById("idade").textContent = p.calcularIdade(2025).toString();
-    
+        document.getElementById("codigo").textContent = p.codigo.toString();
+        document.getElementById("saldo").textContent = p.saldo.toString();
+    });
+
+    btnDeposito.addEventListener("click", ()=>{
+        p.deposita(parseFloat(campoDeposito.value));
+        document.getElementById("saldo").textContent = p.saldo.toString();
+    });
+
+    btnComprar.addEventListener("click", ()=>{
+        if(p.comprar(parseFloat(campoCompra.value))){
+            alert("Obrigado pela compra");
+        }else{
+            alert("Saldo inssuficiente faça um depósito");
+        }
+
+        document.getElementById("saldo").textContent = p.saldo.toString();
     });
     
+
 }
